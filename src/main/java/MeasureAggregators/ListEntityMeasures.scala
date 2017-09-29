4889dde2fd8432424e83fb22fe4f4cd442f8c04b
iosifidis <iosifidis@l3s.de>
2017-09-29 16:06:28 +0200

    Class for multiple entities statistics generator

--- src/main/java/MeasureAggregators/ListEntityMeasures.scala
+++ src/main/java/MeasureAggregators/ListEntityMeasures.scala
@@ -5,12 +5,12 @@ import java.util.Date
 
 import SpamDetection.ProcessingOfRow
 import breeze.linalg.{max, min}
-import breeze.numerics.abs
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import org.joda.time.{DateTime, Period}
-import scala.io.Source
+
 import scala.collection.mutable.HashSet
+import scala.io.Source
 import scala.util.Try
 
 object ListEntityMeasures {
@@ -71,19 +71,22 @@ object ListEntityMeasures {
   }
 
   def main(args: Array[String]) {
-//    val t0 = System.nanoTime()
+    //    val t0 = System.nanoTime()
 
     val processingOfRow: ProcessingOfRow = new ProcessingOfRow
     val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM")
     val sdf2: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
     val sdf3: SimpleDateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy")
     var entitiesList = HashSet[String]()
-    def conf = new SparkConf().setAppName(SingleEntityMeasures.getClass.getName)
+    def conf = new SparkConf().setAppName(this.getClass.getName)
 
     val sc = new SparkContext(conf)
     val argsLen = args.length
-
     sc.setLogLevel("ERROR")
+    if (argsLen != 6){
+      println("invalid number of arguments, must set up to 6. exiting...")
+      exit(1)
+    }
     var startingYear = ""
     var startingMonth = ""
     var startingDay = ""
@@ -171,12 +174,12 @@ object ListEntityMeasures {
     val endDate: Date = sdf.parse(endingYear + "-" + endingMonth)
     val endDateFull: Date = sdf2.parse(endingYear + "-" + endingMonth + "-" + endingDay)
 
-    val myList = dateRange(new DateTime(startDate), new DateTime(endDate), new Period().withMonths(1)).toList.map(_.toDate).map(x => args(argsLen - 1) + sdf.format(x) ).distinct
-    val listConcat = myList.mkString(",")
+    val myList = dateRange(new DateTime(startDate), new DateTime(endDate), new Period().withMonths(1)).toList.map(_.toDate).map(x => sdf.format(x) ).distinct
+    //    val listConcat = myList.mkString(",")
 
     val listSource =  Source.fromFile(args(2))
     for(line <- listSource.getLines() ) {
-      entitiesList.add(line)
+      entitiesList.add(line.replace("\n", ""))
     }
 
     val delta = args(3).toDouble
@@ -184,43 +187,27 @@ object ListEntityMeasures {
 
     println(entitiesList)
 
-//    val indexedRddTemp = sc.textFile(listConcat)
-    val indexedRdd = sc.textFile(listConcat).filter{x=>
-      val temp: Date = sdf3.parse(x.split("\t")(2))
-      temp.getTime >= startDateFull.getTime && temp.getTime <= endDateFull.getTime
-    }.cache
+    myList.foreach{ step=>
 
-    val totalTweets = indexedRdd.count().toDouble
-    val totalUserCnt = indexedRdd.map(_.split("\t")(1)).distinct().count().toDouble
+      println(step)
+      val indexedRdd = sc.textFile(args(argsLen - 1) + step).filter{x=>
+        val temp: Date = sdf3.parse(x.split("\t")(2))
+        temp.getTime >= startDateFull.getTime && temp.getTime <= endDateFull.getTime
+      }.cache
 
-    println("totalTweets " +totalTweets )
+      val totalTweets = indexedRdd.count().toDouble
+      val totalUserCnt = indexedRdd.map(_.split("\t")(1)).distinct().count().toDouble
 
-    val totalEntitySet = indexedRdd.filter{ line=>
-      var flag = false
-      val entity = line.split("\t")(3)
+      println("totalTweets " +totalTweets )
 
-      if (!entity.equals("null;")){
-        val eList = entity.split(";")
-        for(name <- eList) {
-          if (entitiesList.contains(name.split(":")(1))){
-            flag = true
-          }
-        }
-      }
-      flag
-    }.cache()
-
-    for(item <- entitiesList){
-//      val entitySet = totalEntitySet.filter(_.split("\t")(3).contains(item)).cache()
-
-      val entitySet = indexedRdd.filter{ line=>
+      val totalEntitySet = indexedRdd.filter{ line=>
         var flag = false
         val entity = line.split("\t")(3)
 
         if (!entity.equals("null;")){
           val eList = entity.split(";")
           for(name <- eList) {
-            if (name.split(":")(1).equals(item)){
+            if (entitiesList.contains(name.split(":")(1))){
               flag = true
             }
           }
@@ -228,64 +215,77 @@ object ListEntityMeasures {
         flag
       }.cache()
 
-      val entityCnt = entitySet.count().toDouble
-      println(item + " entityCnt " + entityCnt )
+      for(item <- entitiesList) {
+        //      val entitySet = totalEntitySet.filter(_.split("\t")(3).contains(item)).cache()
 
-      val entityUserCnt = entitySet.map(_.split("\t")(1)).distinct().count().toDouble
+        val entitySet = indexedRdd.filter { line =>
+          var flag = false
+          val entity = line.split("\t")(3)
 
+          if (!entity.equals("null;")) {
+            val eList = entity.split(";")
+            for (name <- eList) {
+              if (name.split(":")(1).equals(item)) {
+                flag = true
+              }
+            }
+          }
+          flag
+        }.cache()
 
-      val populT = entityCnt/totalTweets
-      val populU = entityUserCnt/totalUserCnt
+        val entityCnt = entitySet.count().toDouble
+        println(item + " entityCnt " + entityCnt)
 
-      val popularity = populT * populU
-
-      val attitudePosCnt = entitySet.map(_.split("\t")(4).split(" ")(0).toInt).sum()
-      val attitudeNegCnt = entitySet.map(_.split("\t")(4).split(" ")(1).toInt).sum()
-      val sentimentalityCnt = attitudePosCnt - attitudeNegCnt - (2*entityCnt)
-
-      val attitude = (attitudePosCnt + attitudeNegCnt)/entityCnt
-      val sentimentality = sentimentalityCnt/entityCnt
-
-      val controversiality = controversialityForOne(entitySet, entityCnt, delta)
-
-      val myListTemp = List(
-        ("popul_Tweets", populT ),
-        ("popul_Users",populU ),
-        ("popularity", popularity),
-        ("attitude", attitude),
-        ("sentimentality", sentimentality),
-        ("controversiality", controversiality)
-      )
-
-      sc.parallelize(myListTemp).repartition(1).saveAsTextFile(item + "/popularity_" + item + "_" + args(0) + "_" + args(1))
-
-      val entitySetForTopK = entitySet.map(s => s.split("\t")(3)).flatMap(t => t.split(";"))
-        .map(u => u.split(":")(1)).cache()
-        .filter( x => !processingOfRow.isStopword(x) && !x.equals(item))
-
-////////
-      val e1Cnt = entitySetForTopK.count().toDouble
-      val entitySetCnt = sc.parallelize(entitySetForTopK.countByValue().toSeq)
-        .sortBy(k => k._2, ascending = false)
-        .take(topK)
-
-      val approvedEntities = sc.parallelize(entitySetCnt.map{x=>
-        (x._1, calculateCONtNew(e1Cnt, x._2.toDouble))
-      })
-      approvedEntities.repartition(1).saveAsTextFile(item + "/K-Network_" + item + "_" + args(0) + "_" + args(1))
-
-      val E = calculateEplusEminus(entitySet, entitySetForTopK, e1Cnt, delta, topK)
-      sc.parallelize(E._1).repartition(1).saveAsTextFile(item + "/EplusNetwork_"  + item + "_" + args(0) + "_" + args(1))
-      sc.parallelize(E._2).repartition(1).saveAsTextFile(item + "/EminusNetwork_" + item + "_" + args(0) + "_" + args(1))
-/////
+        val entityUserCnt = entitySet.map(_.split("\t")(1)).distinct().count().toDouble
 
 
+        val populT = entityCnt / totalTweets
+        val populU = entityUserCnt / totalUserCnt
 
+        val popularity = populT * populU
 
+        val attitudePosCnt = entitySet.map(_.split("\t")(4).split(" ")(0).toInt).sum()
+        val attitudeNegCnt = entitySet.map(_.split("\t")(4).split(" ")(1).toInt).sum()
+        val sentimentalityCnt = attitudePosCnt - attitudeNegCnt - (2 * entityCnt)
 
+        val attitude = (attitudePosCnt + attitudeNegCnt) / entityCnt
+        val sentimentality = sentimentalityCnt / entityCnt
 
+        val controversiality = controversialityForOne(entitySet, entityCnt, delta)
+
+        val myListTemp = List(
+          ("popul_Tweets", populT),
+          ("popul_Users", populU),
+          ("popularity", popularity),
+          ("attitude", attitude),
+          ("sentimentality", sentimentality),
+          ("controversiality", controversiality)
+        )
+
+        sc.parallelize(myListTemp).repartition(1).saveAsTextFile("ExtractedEntities/" + item + "/popularity_" + step)
+
+        val entitySetForTopK = entitySet.map(s => s.split("\t")(3)).flatMap(t => t.split(";"))
+          .map(u => u.split(":")(1)).cache()
+          .filter(x => !processingOfRow.isStopword(x) && !x.equals(item))
+
+        ////////
+        val e1Cnt = entitySetForTopK.count().toDouble
+        val entitySetCnt = sc.parallelize(entitySetForTopK.countByValue().toSeq)
+          .sortBy(k => k._2, ascending = false)
+          .take(topK)
+
+        val approvedEntities = sc.parallelize(entitySetCnt.map { x =>
+          (x._1, calculateCONtNew(e1Cnt, x._2.toDouble))
+        })
+        approvedEntities.repartition(1).saveAsTextFile("ExtractedEntities/" + item + "/K-Network_" + step)
+
+        val E = calculateEplusEminus(entitySet, entitySetForTopK, e1Cnt, delta, topK)
+        sc.parallelize(E._1).repartition(1).saveAsTextFile("ExtractedEntities/" +item + "/EplusNetwork_" + step)
+        sc.parallelize(E._2).repartition(1).saveAsTextFile("ExtractedEntities/" +item + "/EminusNetwork_"+ step)
+
+      }
     }
-//    println("Elapsed time: " + (System.nanoTime() - t0) + "ns")
+    //    println("Elapsed time: " + (System.nanoTime() - t0) + "ns")
 
   }
 
